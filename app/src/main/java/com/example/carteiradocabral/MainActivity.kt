8c75fc4ed31e8acfd768c6b9af7baf27dbbe4dcb
Carlos Cabral <carloscabral@google.com>
2026-02-10 17:17:59 -0300

    feat(app): Update MainActivity and create CarteiraTheme

--- app/src/main/java/com/example/carteiradocabral/MainActivity.kt
+++ app/src/main/java/com/example/carteiradocabral/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.carteiradocabral
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import com.example.carteiradocabral.ui.screens.CalculatorScreen
+import com.example.carteiradocabral.ui.theme.CarteiraTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,10 +12,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface {
-                    Text("Hello Hilt")
-                }
+            CarteiraTheme {
+                CalculatorScreen()
             }
         }
     }
---
+++ app/src/main/java/com/example/carteiradocabral/ui/theme/Theme.kt
@@ -0,0 +1,42 @@
+package com.example.carteiradocabral.ui.theme
+
+import android.app.Activity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val LightColorScheme = lightColorScheme(
+    primary = Primary,
+    secondary = Secondary,
+    background = Background,
+    surface = Surface,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = TextMain,
+    onSurface = TextMain,
+)
+
+@Composable
+fun CarteiraTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = LightColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
